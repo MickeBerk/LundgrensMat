e2204f6cad27d02f962751761f967cdae3e54ce7
Micke <mikael.bjerkerot@gmail.com>
2016-12-12 10:08:22 +0100

    cleaned up some code in single.php

--- understrap/single.php
+++ understrap/single.php
@@ -4,9 +4,8 @@
  *
  * @package understrap
  */
-?>
-<?php acf_form_head(); ?>
-<?php get_header(); ?>
+
+get_header(); ?>
 <div class="wrapper" id="single-wrapper">
     
     <div  id="content" class="container">
@@ -32,22 +31,6 @@
                         
                     <?php endwhile; // end of the loop. ?>
 
-                        <?php acf_form( $options = array(
-            'id'                    => 'ad-form',
-            'post_id'               => false,
-            'new_post'              => false,
-            'field_groups'          => false,
-            'fields'                => false,
-            'post_title'            => false,
-            'post_content'          => false,
-            'form'                  => true,
-            'submit_value'          => __("Uppdatera recept", 'acf'),
-            'label_placement'       => 'top',
-            'instruction_placement' => 'label',
-            'field_el'              => 'div',
-            'uploader'              => 'wp',
-            'honeypot'              => true
-            )); ?>
                 </main><!-- #main -->
                 
             </div><!-- #primary -->
