e065514cb83354d9c7d5b62a429add1d6d4a5010
Micke <mikael.bjerkerot@gmail.com>
2017-01-10 13:33:01 +0100

    Some css and html changes

--- lundgrensmat/page-templates/homepage.php
+++ lundgrensmat/page-templates/homepage.php
@@ -6,67 +6,51 @@
  *
  * @package understrap
  */
-
 get_header(); ?>
 
 <div class="wrapper" id="full-width-page-wrapper">
-
     <div  id="content" class="container-fluid">
     <div class="col-md-12 main-bg-wrapper">
         <div class="main-bg"></div>
-    </div>
-        
+    </div>        
 	   <div id="primary" class="col-md-12 content-area">
-
             <main id="main" class="site-main" role="main">
                     <?php while ( have_posts() ) : the_post(); ?>
-
                         <?php get_template_part( 'loop-templates/content', 'page' ); ?>
-                    <div class="three_latest_posts">
-                        <?php 
+                    <?php
+                        if ( comments_open() || get_comments_number() ) :
+                            comments_template();                       
+                        endif;
+                    ?>
+                <?php endwhile; // end of the loop. ?>
+            </main><!-- #main -->
+	    </div><!-- #primary -->    
 
-                        $posts = get_posts(array(
-                            'posts_per_page'    => 3,
-                            'post_type'         => 'recept'
-                        ));
-
-                        if( $posts ): ?>
-                            
-                            <ul>
-                                
-                            <?php foreach( $posts as $post ): 
-                                
-                                setup_postdata( $post );
-                                
-                                ?>
+    </div><!-- Container end -->  
+    <div class="container">
+    <div class="row">
+            <div id="three_latest_posts" class="col-md-12">
+                <?php 
+                    $posts = get_posts(array(
+                        'posts_per_page'    => 3,
+                        'post_type'         => 'recept'
+                    ));
+                    if( $posts ): ?>
+                        <ul>                              
+                        <?php foreach( $posts as $post ):                                
+                            setup_postdata( $post ); ?>
+                            <div class="each-recept col-md-4 col-xs-12">
                                 <li>
                                     <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><br>
                                     <p><?php the_field('receptbeskrivning'); ?></p>
                                 </li>
-                           
-                            <?php endforeach; ?>
-                            
-                            </ul>
-                            
+                            </div>
+                        <?php endforeach; ?>                       
+                        </ul>                           
                             <?php wp_reset_postdata(); ?>
-
-                        <?php endif; ?>
-                    </div>
-                    <?php
-                        if ( comments_open() || get_comments_number() ) :
-
-                            comments_template();
-                        
-                        endif;
-                    ?>
-
-                <?php endwhile; // end of the loop. ?>
-
-            </main><!-- #main -->
-	    </div><!-- #primary -->
-        
-    </div><!-- Container end -->
-    
+                    <?php endif; ?>
+            </div>
+        </div>
+        </div>  
 </div><!-- Wrapper end -->
-
 <?php get_footer(); ?>
